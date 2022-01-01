00ed313ce7573ecc6251daafa303dd770eaa4a38
Hunobas <po127992@gmail.com>
2022-01-01 16:31:04 +0900

    5. Longest Palindromic Substring : O(n^2)으로 구현. 아래 힌트 사용 하나의 중심 char를 잡아서 그것을 중심으로 palindrome인지 확인.
    *허나 1000자의 똑같은 알파벳은 시간이 너무 오래 걸린다고 리젝을 먹음. 땜방코드로 기웠지만,
    *다른 알파벳으로 이루어진 1000자 palindrome은 해결 못함.

--- LeetCode_Test/LongestPalindromicSubstring/LongestPalindromicSubstring.cpp
+++ LeetCode_Test/LongestPalindromicSubstring/LongestPalindromicSubstring.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <vector>
 #include <cctype>
 
 using std::cout;
@@ -11,45 +10,58 @@ class Solution {
 public:
     string longestPalindrome(string s) {
 
+
         if (s.size() < 1 || s.size() > 1000)
             return "";
+
+        int isConsistOfOneChar = 0;
         for (auto c : s)
+        {
+            if (c == s[0])
+                isConsistOfOneChar++;
+
             if (isdigit(c) || isalpha(c))
                 continue;
             else
                 return "";
+        }
+        if (isConsistOfOneChar == s.size())
+            return s;
 
-        string result{ s[0] };
-        
-        for (int i = 0; i < s.size() - 1; ++i)
+        string result = "";
+        int resultLen = result.size();
+
+        for (int i = 0; i < s.size(); ++i)
         {
-            for (int j = s.size() - 1; j > i; --j)
+            int iLeft = i;
+            int iRight = i;
+
+            // odd palindrome
+            while (iLeft >= 0 && iRight < s.size() && s[iLeft] == s[iRight])
             {
-                string temp = "";
-
-                if (s[i] == s[j])
+                string temp = s.substr(iLeft, iRight - iLeft + 1);
+                if (temp.size() > resultLen)
                 {
-                    int itmp = i;
-                    int jtmp = j;
-
-                    while (itmp < jtmp)
-                    {
-                        itmp++; jtmp--;
-
-                        if (s[itmp] == s[jtmp])
-                            continue;
-                        else
-                            goto NotPalindrome;
-                    }
-
-                    for (int k = i; k <= j; ++k)
-                        temp += s[k];
-                    result = temp.size() > result.size() ? temp : result;
-                    if (result.size() > s.size() / 2)
-                        return result;
+                    result = temp;
+                    resultLen = temp.size();
                 }
-                
-            NotPalindrome:;
+                iLeft--;
+                iRight++;
+            }
+
+            // even palindrome
+            iLeft = i;
+            iRight = i + 1;
+            while (iLeft >= 0 && iRight < s.size() && s[iLeft] == s[iRight])
+            {
+                string temp = s.substr(iLeft, iRight - iLeft + 1);
+                if (temp.size() > resultLen)
+                {
+                    result = temp;
+                    resultLen = temp.size();
+                }
+                iLeft--;
+                iRight++;
             }
         }
 
@@ -59,10 +71,19 @@ public:
 
 int main()
 {
-    string s1 = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb";
+    string s1 = "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
+    string s2 = "abbaabccba";
+    string s3 = "sibalama";
+    string s4 = "whattahw";
+    string s5 = "ababcccbaba";
+
 
     Solution sol;
 
     cout << sol.longestPalindrome(s1) << endl;
+    cout << sol.longestPalindrome(s2) << endl;
+    cout << sol.longestPalindrome(s3) << endl;
+    cout << sol.longestPalindrome(s4) << endl;
+    cout << sol.longestPalindrome(s5) << endl;
 	return 0;
 }
