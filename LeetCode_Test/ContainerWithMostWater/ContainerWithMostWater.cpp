99e0d05b778f69c7e6b444db2f60e68eb7d263f7
Hunobas <po127992@gmail.com>
2022-01-04 15:42:03 +0900

    11. Container With Most Water : most water이 되는 왼쪽과 오른쪽 후보군을 골라 O(n)으로 구현.
    허나 예외들이 분명 많이 발생할 것이고, 본인은 [8,10,14,0,13,10,9,9,11,11]에서 오류가 나면서 실패

--- LeetCode_Test/ContainerWithMostWater/ContainerWithMostWater.cpp
+++ LeetCode_Test/ContainerWithMostWater/ContainerWithMostWater.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using std::vector;
 using std::cout;
 using std::endl;
+using std::max_element;
 
 class Solution {
 public:
@@ -16,27 +18,55 @@ public:
             if (i < 0 || i > pow(10, 4))
                 return 0;
 
-        int res = 0;
+        int half = n / 2;
+        vector<float> left;
+        vector<float> right;
+        left.reserve(half + 2);
+        right.reserve(half + 2);
+        float distance = 0;
+        float center = (n - 1) / 2.0f;
 
-        for (int i = 0; i < n - 1; ++i)
+        for (int i = 0; i < half; ++i)
         {
-            for (int j = i + 1; j < n; ++j)
-            {
-                int water = (j - i) * std::min(height[i], height[j]);
-                if (water > res)
-                    res = water;
-            }
+            distance = (center - i) + height[i];
+            left.push_back(distance);
+        }
+        for (int i = half; i < n; ++i)
+        {
+            distance = -(center - i) + height[i];
+            right.push_back(distance);
         }
 
-        return res;
+        auto temp = left;
+        auto lmax = max_element(temp.begin(), temp.end()) - temp.begin();
+        temp[lmax] = 0;
+        auto lsec = --max_element(temp.rbegin(), temp.rend()).base() - temp.begin();
+
+        temp = right;
+        auto rmax = --max_element(temp.rbegin(), temp.rend()).base() - temp.begin();
+        temp[rmax] = 0;
+        auto rsec = max_element(temp.begin(), temp.end()) - temp.begin();
+
+        vector<int> res(6);
+        res.push_back((half + rmax - lmax) * std::min(height[lmax], height[half + rmax]));
+        res.push_back((half + rmax - lsec) * std::min(height[lsec], height[half + rmax]));
+        res.push_back((half + rsec - lmax) * std::min(height[lmax], height[half + rsec]));
+        res.push_back((half + rsec - lsec) * std::min(height[lsec], height[half + rsec]));
+        res.push_back((std::abs(lmax - lsec)) * std::min(height[lmax], height[lsec]));
+        res.push_back((std::abs(rmax - rsec)) * std::min(height[half + rmax], height[half + rsec]));
+
+        return *max_element(res.begin(), res.end());
     }
 };
 
 int main()
 {
-    vector<int> height{ 1 };
+    vector<int> height1{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // 25
+    vector<int> height2{ 2, 3, 4, 5, 18, 17, 6 }; // 17
+    vector<int> height3{ 8, 10, 14, 0, 13, 10, 9, 9, 11, 11 }; // 80
     Solution sol;
 
-    cout << sol.maxArea(height) << endl;
-    
+    cout << sol.maxArea(height1) << endl;
+    cout << sol.maxArea(height2) << endl;
+    cout << sol.maxArea(height3) << endl;
 }
